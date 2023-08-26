effe39871c2cb71d8025788edd7fca414d1c4da5
Luis Alvarez <iluisdat@gmail.com>
2023-08-26 02:27:38 -0300

    footer improvement

--- src/components/main-footer.tsx
+++ src/components/main-footer.tsx
@@ -19,7 +19,7 @@ const Footer = () => {
           <ul className="flex justify-start space-x-4 my-2">
             {paths.map(({ label, href }) => (
               <li key={href}>
-                <Link href={href} className="font-light">
+                <Link href={href} className="font-light hover:text-black">
                   {label}
                 </Link>
               </li>
@@ -29,7 +29,7 @@ const Footer = () => {
 
         <p className="text-xs text-center ">
           &#127758;
-          <span className="text-xs font-light leading-none">{` Argentina | Brasil, ${new Date().getFullYear()}`}</span>
+          <span className="text-xs font-light tracking-wide leading-none">{` Argentina | Brasil, ${new Date().getFullYear()}`}</span>
         </p>
       </Container>
     </footer>
