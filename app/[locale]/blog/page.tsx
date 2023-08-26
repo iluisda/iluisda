6955eb0f04e6848b5eb60ceb5c7618dfb6d04ae3
Luis Alvarez <iluisdat@gmail.com>
2023-08-26 00:32:50 -0300

    rive animation astro

--- app/[locale]/blog/page.tsx
+++ app/[locale]/blog/page.tsx
@@ -1,4 +1,7 @@
 "use client";
+import React from "react";
+import { PageWrapper } from "@/components/common/page-wrapper";
+import RiveComponent, { Alignment, Fit, Layout } from "@rive-app/react-canvas";
 import { compareDesc, format, parseISO } from "date-fns";
 import { allPosts, Post } from "contentlayer/generated";
 const blog = () => {
@@ -6,6 +9,21 @@ const blog = () => {
     compareDesc(new Date(a.date), new Date(b.date))
   );
   console.log(posts);
-  return <></>;
+  return (
+    <React.Fragment>
+      <PageWrapper>
+        <RiveComponent
+          src="../../floating_astro.riv"
+          className="h-screen max-h-[30rem]"
+          layout={
+            new Layout({
+              fit: Fit.Contain,
+              alignment: Alignment.TopCenter,
+            })
+          }
+        />
+      </PageWrapper>
+    </React.Fragment>
+  );
 };
 export default blog;
