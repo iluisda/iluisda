45c5cde14b8e5e3e5cccb0f18aa343dc64e8a7a7
Luis Alvarez <iluisdat@gmail.com>
2024-02-21 17:14:21 -0300

    improvements

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -3,78 +3,49 @@ import { PageWrapper } from "@/components/common/page-wrapper";
 import Hero from "@/components/main-home";
 import React from "react";
 import { useTranslations } from "next-intl";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
+import { ScrollText, GalleryVerticalEnd, Rss } from "lucide-react";
+import Text from "@/components/common/text";
+import Link from "next-intl/link";
 
 const Home = () => {
   const t = useTranslations("Index");
+  const p = useTranslations("Paths");
   return (
     <PageWrapper>
       <Hero welcome={t("welcome")} sign={t("sign")} sentence={t("sentence")} />
-      {/* <div className="grid gap-4 grid-cols-2 mt-10">
-          <div className="blur-none">
-            <Card className="backdrop-blur-xl bg-white/30 ">
-              <CardHeader>
-                <CardTitle>Card Title</CardTitle>
-                <CardDescription>Card Description</CardDescription>
-              </CardHeader>
-              <CardContent>
-                <p>Card Content</p>
-              </CardContent>
-              <CardFooter>
-                <p>Card Footer</p>
-              </CardFooter>
-            </Card>
-          </div>
-          <div>
-            <Card className="backdrop-blur-xl bg-white/30 ">
-              <CardHeader>
-                <CardTitle>Card Title</CardTitle>
-                <CardDescription>Card Description</CardDescription>
-              </CardHeader>
-              <CardContent>
-                <p>Card Content</p>
-              </CardContent>
-              <CardFooter>
-                <p>Card Footer</p>
-              </CardFooter>
-            </Card>
-          </div>
-          <div>
-            <Card className="backdrop-blur-xl bg-white/30 ">
-              <CardHeader>
-                <CardTitle>Card Title</CardTitle>
-                <CardDescription>Card Description</CardDescription>
-              </CardHeader>
-              <CardContent>
-                <p>Card Content</p>
-              </CardContent>
-              <CardFooter>
-                <p>Card Footer</p>
-              </CardFooter>
-            </Card>
-          </div>
-          <div>
-            <Card className="backdrop-blur-xl bg-white/30 ">
-              <CardHeader>
-                <CardTitle>Card Title</CardTitle>
-                <CardDescription>Card Description</CardDescription>
-              </CardHeader>
-              <CardContent>
-                <p>Card Content</p>
-              </CardContent>
-              <CardFooter>
-                <p>Card Footer</p>
-              </CardFooter>
-            </Card>
-          </div>
-        </div> */}
+      <div className="grid gap-4 grid-cols-4 mt-10">
+        <Link href={`/cv`}>
+          <Card className="backdrop-blur-xl bg-white/30 shadow">
+            <CardContent className="py-3">
+              <Text className="flex justify-normal">
+                <ScrollText />
+                <span className="ml-3 uppercase">{p("cv")}</span>
+              </Text>
+            </CardContent>
+          </Card>
+        </Link>
+        <Link href={`/projects`}>
+          <Card className="backdrop-blur-xl bg-white/30 shadow">
+            <CardContent className="py-3">
+              <Text className="flex justify-normal">
+                <GalleryVerticalEnd />
+                <span className="ml-3 capitalize">{p("projects")}</span>
+              </Text>
+            </CardContent>
+          </Card>
+        </Link>
+        <Link href={`/blog`}>
+          <Card className="backdrop-blur-xl bg-white/30 shadow">
+            <CardContent className="py-3">
+              <Text className="flex justify-normal">
+                <Rss />
+                <span className="ml-3 capitalize">{p("blog")}</span>
+              </Text>
+            </CardContent>
+          </Card>
+        </Link>
+      </div>
     </PageWrapper>
   );
 };
--- src/components/blog/Post/index.tsx
+++ src/components/blog/Post/index.tsx
@@ -1,3 +1,4 @@
+import { Card } from "@/components/ui/card";
 import { format } from "date-fns";
 import { useTranslations } from "next-intl";
 import Image from "next/image";
@@ -5,47 +6,54 @@ import Link from "next/link";
 const PostArticle = ({ post }: any) => {
   const t = useTranslations("Blog");
   return (
-    <div className="group cursor-pointer flex items-center justify-start overflow-hidden transition-all hover:scale-105 relative opacity-100 transform perspective-1200 my-10">
-      <div className="container mx-auto flex flex-col sm:flex-row items-start">
-        <div className="w-full sm:w-auto">
-          <Link
-            className="block w-full overflow-hidden"
-            href={`/${post.lang}/blog/${post.slug}`}
-          >
-            <div className="p-4">
-              <p className="text-gray-600">
-                <time>{`${format(new Date(post.date), "MMM dd, yyyy")}`}</time>
-                <span aria-hidden="true" className="mx-1">
-                  &middot;
-                </span>
-                <span>
-                  {Math.round(post.readingTime.minutes)} {t("reads")}
-                </span>
-              </p>
-              <h2 className="text-black font-bold">{post.title}</h2>
-              <p className="text-gray-700">{post.excerpt}</p>
+    <div className="group rounded-lg shadow-sm cursor-pointer flex items-center justify-start overflow-hidden transition-all hover:scale-105 relative opacity-100 transform perspective-1200 my-10">
+      <div className="blur-none">
+        <Card className="backdrop-blur-xl bg-white/30 ">
+          <div className="container mx-auto flex flex-col sm:flex-row items-start">
+            <div className="w-full sm:w-auto">
+              <Link
+                className="block w-full overflow-hidden"
+                href={`/${post.lang}/blog/${post.slug}`}
+              >
+                <div className="p-4">
+                  <p className="text-gray-600">
+                    <time>{`${format(
+                      new Date(post.date),
+                      "MMM dd, yyyy"
+                    )}`}</time>
+                    <span aria-hidden="true" className="mx-1">
+                      &middot;
+                    </span>
+                    <span>
+                      {Math.round(post.readingTime.minutes)} {t("reads")}
+                    </span>
+                  </p>
+                  <h2 className="text-black font-bold">{post.title}</h2>
+                  <p className="text-gray-700">{post.excerpt}</p>
+                </div>
+              </Link>
             </div>
-          </Link>
-        </div>
-        <div className="custom-w-rem custom-h-rem ml-4 hidden sm:block">
-          <Link
-            className=" overflow-hidden"
-            href={`/${post.lang}/blog/${post.slug}`}
-          >
-            <Image
-              width={175}
-              height={100}
-              src={post.image || "/images/default_cover.jpg"}
-              {...(post.image && {
-                placeholder: "blur",
-                blurDataURL: post.image,
-              })}
-              alt={post.image || "Thumbnail"}
-              priority={true}
-              className="w-full h-full object-cover rounded-lg shadow-md"
-            />
-          </Link>
-        </div>
+            <div className="custom-w-rem custom-h-rem ml-4 my-4 hidden sm:block">
+              <Link
+                className=" overflow-hidden"
+                href={`/${post.lang}/blog/${post.slug}`}
+              >
+                <Image
+                  width={175}
+                  height={100}
+                  src={post.image || "/images/default_cover.jpg"}
+                  {...(post.image && {
+                    placeholder: "blur",
+                    blurDataURL: post.image,
+                  })}
+                  alt={post.image || "Thumbnail"}
+                  priority={true}
+                  className="w-full h-full object-cover rounded-lg"
+                />
+              </Link>
+            </div>
+          </div>
+        </Card>
       </div>
     </div>
   );
